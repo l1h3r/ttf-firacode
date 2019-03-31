a7be4fc0086d683543e2ba08c4c714f4612ad8ad
l1h3r <devin.turner09@gmail.com>
2019-03-31 15:36:01 -0700

    Initial version

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+/// Fira Code - Bold
+#[cfg(feature = "bold")]
+pub const BOLD: &'static [u8] = include_bytes!("../assets/FiraCode-Bold.ttf");
+
+/// Fira Code - Light
+#[cfg(feature = "light")]
+pub const LIGHT: &'static [u8] = include_bytes!("../assets/FiraCode-Light.ttf");
+
+/// Fira Code - Medium
+#[cfg(feature = "medium")]
+pub const MEDIUM: &'static [u8] = include_bytes!("../assets/FiraCode-Medium.ttf");
+
+/// Fira Code - Regular
+#[cfg(feature = "regular")]
+pub const REGULAR: &'static [u8] = include_bytes!("../assets/FiraCode-Regular.ttf");
+
+/// Fira Code - Retina
+#[cfg(feature = "retina")]
+pub const RETINA: &'static [u8] = include_bytes!("../assets/FiraCode-Retina.ttf");
